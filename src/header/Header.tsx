c06153bf2b358db3340a07b6fad992b0c425ac07
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-04-19 20:25:51 +0200

    add header for mobile

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './header.module.css'
 import Navigation from "./navigation/Navigation";
-import wreathImg from '../assets/images/wreath.png'
+import wreathImg from '../assets/images/wreathImg.png'
 
 const Header = () => {
     const wreath = {
@@ -13,12 +13,7 @@ const Header = () => {
             <div className={style.easterBlock}>
                 <p>Что все вокруг празднуют, и почему это важно.</p>
                 <h5>ЧТО ТАКОЕ</h5>
-               <div className={style.circle1}/>
-               <div className={style.circle2}/>
-               <div className={style.circle3}/>
-               <div className={style.circle4}/>
                 <div className={style.oriol1}/>
-                <div className={style.oriol2}/>
                 <div className={style.wreathBlock} style={wreath}>
                     <h1>ПАСХА</h1>
                 </div>
